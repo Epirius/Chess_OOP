5a03459f4c7e97e1c315da1ea72dd4c8990356a8
Epirius <68005825+Epirius@users.noreply.github.com>
2022-02-21 11:35:16 +0100

    created tests for Direction enum

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,12 +1,12 @@
 public enum Direction {
-    North(8),
-    East(1),
-    South(-8),
-    West(-1),
-    NorthEast(9),
-    NorthWest(7),
-    SouthEast(-7),
-    SouthWest(-9);
+    NORTH(8),
+    EAST(1),
+    SOUTH(-8),
+    WEST(-1),
+    NORTHEAST(9),
+    NORTHWEST(7),
+    SOUTHEAST(-7),
+    SOUTHWEST(-9);
 
     final int directionId;
 
@@ -14,14 +14,16 @@ public enum Direction {
         this.directionId = directionIdd;
     }
 
-    public int getDirection(int currentSquare, Direction direction){
+    public int getDirection(int currentSquare){
+        if ( currentSquare + directionId > 63 || currentSquare + directionId < 0){throw new IndexOutOfBoundsException("current square " + currentSquare + " + " + directionId + " = " + (currentSquare + directionId) + " which is out of bounds.");}
+
         int currentSquareY = IBoard.squareToCoordinates(currentSquare)[1];
         int newSquareY = IBoard.squareToCoordinates(currentSquare + directionId)[1];
 
         if (directionId > 1 && currentSquareY + 1 == newSquareY){return currentSquare + directionId;}
         else if (directionId < 2 && directionId > -2 && currentSquareY == newSquareY){return  currentSquare + directionId;}
         else if (directionId < -1 && currentSquareY -1 == newSquareY){return  currentSquare + directionId;}
-        else {throw new IndexOutOfBoundsException("current y is: " + currentSquareY + "| new y is: " + newSquareY);}
+        else {throw new IndexOutOfBoundsException("current y is: " + currentSquareY + " new y is: " + newSquareY + " which it should not be");}
 
 
     }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+
 /**
  * @author Felix Kaasa
  */
@@ -565,4 +566,49 @@ public class test {
         List<Square> test1 = squaresBetween(0, 7, board);
         Assert.assertEquals(0, test1.size());
     }*/
+
+    @Test
+    public void testDirectionEnum(){
+        //checking all directions in the middle of the board:
+        Assert.assertEquals(36, Direction.NORTH.getDirection(28));
+        Assert.assertEquals(37, Direction.NORTHEAST.getDirection(28));
+        Assert.assertEquals(29, Direction.EAST.getDirection(28));
+        Assert.assertEquals(21, Direction.SOUTHEAST.getDirection(28));
+        Assert.assertEquals(20, Direction.SOUTH.getDirection(28));
+        Assert.assertEquals(19, Direction.SOUTHWEST.getDirection(28));
+        Assert.assertEquals(27, Direction.WEST.getDirection(28));
+        Assert.assertEquals(35, Direction.NORTHWEST.getDirection(28));
+
+        //checking at the top edge
+        try {Direction.NORTH.getDirection(60);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+        try {Direction.NORTHEAST.getDirection(60);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+        try {Direction.NORTHWEST.getDirection(60);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+
+        //checking at the bottom edge
+        try {Direction.SOUTH.getDirection(3);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+        try {Direction.SOUTHWEST.getDirection(3);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+        try {Direction.SOUTHEAST.getDirection(3);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+
+        //checking at the left edge
+        try {Direction.WEST.getDirection(24);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+        try {Direction.NORTHWEST.getDirection(24);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+        try {Direction.SOUTHWEST.getDirection(24);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+
+        //checking at the right edge
+        try {Direction.EAST.getDirection(31);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+        try {Direction.NORTHEAST.getDirection(31);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+        try {Direction.SOUTHEAST.getDirection(31);}
+        catch (Throwable ex) {Assert.assertTrue(ex instanceof IndexOutOfBoundsException);}
+    }
 }
