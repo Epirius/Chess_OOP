8ee022b884a789fc9e6f657125123d2fcedeb366
Epirius <68005825+Epirius@users.noreply.github.com>
2022-02-23 13:42:18 +0100

    .

--- src/main/java/Main/Constants.java
+++ src/main/java/Main/Constants.java
@@ -1,6 +1,10 @@
 package Main;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public final class Constants {
     public static final int squareSize = 64;
@@ -13,5 +17,26 @@ public final class Constants {
     public static final Color colorBackground = new Color(90, 94, 89);
     public static final Color colorDarkSquare = new Color(21, 29, 36);
     public static final Color colorLightSquare = new Color(237, 132, 99);
+
+    // Images
+    public static  BufferedImage pawnW;
+    public static  BufferedImage rookW;
+    public static  BufferedImage knightW;
+    public static  BufferedImage bishopW;
+    public static  BufferedImage queenW;
+    public static  BufferedImage kingW;
+
+    static {
+        try {
+            pawnW = ImageIO.read(new File("/images/pawnW.png"));
+            rookW = ImageIO.read(new File("/images/rookW.png"));
+            knightW = ImageIO.read(new File("/images/knightW.png"));
+            bishopW = ImageIO.read(new File("/images/bishopW.png"));
+            queenW = ImageIO.read(new File("/images/queenW.png"));
+            kingW = ImageIO.read(new File("/images/kingW.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
