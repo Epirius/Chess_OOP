6916fb8503e8d468e17cf9d909da3ab01e2f7b80
Felix <mrKaasa@protonmail.com>
2022-04-25 18:36:21 +0200

    added tests for view

---
+++ src/test/java/View/testView.java
@@ -0,0 +1,28 @@
+package View;
+
+/**
+ * @author Felix Kaasa
+ */
+public class testView {
+
+    // Instructions for manual testing of buttons
+
+    /*
+        Starting screen:
+        -make sure the highlighting changes based on which "Choose a team" button is clicked
+        -test that the pluss and minus buttons change the text above.
+        -test that create game send you to the game.
+        -test that selecting the white piece during "choose a team" makes the ai control the black pieces
+        -test that selecting the black piece during "choose a team" makes the ai control the white pieces
+        -test that selecting the black and white piece during "choose a team" makes the player control both sides
+
+        During the game:
+        -make sure clicking a piece that can move displayes the possible moves.
+        -make sure that nothing happens if you click on a piece that can't move.
+        -make sure the clock works when playing.
+
+        Game over:
+        -make sure clicking main menu button sends you back to the starting game screen.
+        -make sure clicking the quit button quits the program
+     */
+}
